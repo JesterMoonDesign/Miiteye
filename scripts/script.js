b52251d5c19c7811b8fb0c6fe1479c7985fe39b8
JesterMoonDesign <82544432+JesterMoonDesign@users.noreply.github.com>
2021-07-27 14:39:19 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -81,7 +81,7 @@ function section4Slider() {
 			function onDrag (event) {
 				event.preventDefault();
 
-				let x2 = event.changedTouches[0].clientX * 0.7;
+				let x2 = event.changedTouches[0].clientX * 0.6;
 				let translateX = index * slideWidth;
 
 				if (x1 - x2 > 0) {
